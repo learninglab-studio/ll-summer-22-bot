e98ee041faefc3800c112d30f1b1fac27e344d0f
mk <mk@mks-MacBook-Air.local>
2022-07-18 15:03:08 -0400

    Updates:

--- src/slashes/stills-slash.js
+++ src/slashes/stills-slash.js
@@ -5,7 +5,7 @@ const stillsSlash = async ({ command, ack, respond }) => {
     // Acknowledge command request
     await ack();
     let result = yargs(command.text).parse()
-    await respond(`need stills for:${JSON.stringify(command, null, 4)}`);
+    await respond(makeBlocks(command));
     sendToAirtable({
         record: {
             "SlackTs": "command.event_ts or similar",
@@ -16,6 +16,38 @@ const stillsSlash = async ({ command, ack, respond }) => {
         },
         table: "StillsRequests"
     })
+}
+
+const makeBlocks = () => {
+    return {
+        blocks: [
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "PGM1",
+                            "emoji": true
+                        },
+                        "value": "clicked_PGM1",
+                        "action_id": "actionId-0"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "PGM2",
+                            "emoji": true
+                        },
+                        "value": "clicked_PGM2",
+                        "action_id": "actionId-1"
+                    }
+                ]
+            },
+        ]
+    }
 }
 
 module.exports = stillsSlash
