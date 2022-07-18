50a1d8d87f25235195cd8738a56bc911a30c3612
mk <mk@dhcp-10-250-253-232.harvard.edu>
2022-07-18 12:39:27 -0400

    Updates:

--- src/slashes/stills-slash.js
+++ src/slashes/stills-slash.js
@@ -5,7 +5,7 @@ const stillsSlash = async ({ command, ack, respond }) => {
     // Acknowledge command request
     await ack();
     let result = yargs(command.text).parse()
-    await respond(`here you go:\n${JSON.stringify(result, null, 4)}`);
+    await respond(`need stills for:${JSON.stringify(command, null, 4)}`);
 }
 
 module.exports = stillsSlash
